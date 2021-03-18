1ec10fd216d4ff8353854757ac2f8afbf716eaf8
Chance Swartz <71352443+chance10113@users.noreply.github.com>
2021-03-18 16:53:48 -0500

    Reached MVP

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,27 +1,33 @@
-import React from 'react';
+import React, { useContext } from "react";
 
 // Components
-import Item from './ShoppingCartItem';
+import Item from "./ShoppingCartItem";
 
-const ShoppingCart = props => {
-	const getCartTotal = () => {
-		return props.cart.reduce((acc, value) => {
-			return acc + value.price;
-		}, 0).toFixed(2);
-	};
+// Contexts
+import { CartContext } from "../contexts/cartContext";
 
-	return (
-		<div className="shopping-cart">
-			{props.cart.map(item => (
-				<Item key={item.id} {...item} />
-			))}
+const ShoppingCart = (props) => {
+  const { cart } = useContext(CartContext);
+  const getCartTotal = () => {
+    return cart
+      .reduce((acc, value) => {
+        return acc + value.price;
+      }, 0)
+      .toFixed(2);
+  };
 
-			<div className="shopping-cart__checkout">
-				<p>Total: ${getCartTotal()}</p>
-				<button>Checkout</button>
-			</div>
-		</div>
-	);
+  return (
+    <div className="shopping-cart">
+      {cart.map((item) => (
+        <Item key={item.id} {...item} />
+      ))}
+
+      <div className="shopping-cart__checkout">
+        <p>Total: ${getCartTotal()}</p>
+        <button>Checkout</button>
+      </div>
+    </div>
+  );
 };
 
 export default ShoppingCart;
