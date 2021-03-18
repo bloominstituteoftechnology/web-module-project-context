85c9368709703f444bde4b2d6fb56d3d7eeb8c89
L-Steinmacher <dasai.sacul@gmail.com>
2021-03-18 13:57:33 -0700

    first commit,working on event handeler

--- src/App.js
+++ src/App.js
@@ -6,27 +6,36 @@ import data from './data';
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
+import { ProductContext } from './contexts/ProductContext'
+import { CartContext } from './contexts/CartContext'
 
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
-		// add the given item to the cart
+		// //add the given item to the shopping cart
+		// preventDefault()
+		console.log('Cart: ', cart,'Item: ', item)
+		setCart( ...cart, [item])
 	};
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
-
-			{/* Routes */}
-			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
-
-			<Route path="/cart">
-				<ShoppingCart cart={cart} />
-			</Route>
+			<ProductContext.Provider value={{ products, addItem }} >
+				<CartContext.Provider value={{cart}}>
+					<Navigation  />
+				
+					{/* Routes */}
+					<Route exact path="/">
+						<Products />
+					</Route>
+				
+					<Route path="/cart">
+						<ShoppingCart  />
+					</Route>
+				</CartContext.Provider>
+			</ProductContext.Provider>
 		</div>
 	);
 }
