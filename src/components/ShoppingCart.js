d232779d40f88fb9b67189acdfd73c6eabce6a45
Eduardo Lopez <ed98lopez@gmail.com>
2021-03-18 15:40:50 -0700

    MVP

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {CartContext} from '../context/CartContext'
 
 // Components
 import Item from './ShoppingCartItem';
 
 const ShoppingCart = props => {
+
+	const cart = useContext(CartContext)
+	
 	const getCartTotal = () => {
-		return props.cart.reduce((acc, value) => {
+		return cart.reduce((acc, value) => {
 			return acc + value.price;
 		}, 0).toFixed(2);
 	};
