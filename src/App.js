b4a94aa6243dfd035cc1a8a6bfe752098b637380
Molly Brown <mollybrown0516@gmail.com>
2021-04-29 21:29:20 -0400

    product context

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import data from './data';
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
+import ProductContext from './contexts/ProductContext';
 
 function App() {
 	const [products] = useState(data); //one state property
@@ -20,16 +21,18 @@ function App() {
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} /> 
+			<ProductContext.Provider value={{ products, addItem }}>
+				<Navigation cart={cart} /> 
 
-			{/* Routes */}
-			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
+				{/* Routes */}
+				<Route exact path="/">
+					<Products />
+				</Route>
 
-			<Route path="/cart">
-				<ShoppingCart cart={cart} />
-			</Route>
+				<Route path="/cart">
+					<ShoppingCart cart={cart} />
+				</Route>
+			</ProductContext.Provider>
 		</div>
 	);
 }
---
+++ src/contexts/notes.md
@@ -0,0 +1,19 @@
+## Refactor to Context API
+
+1) Create contexts subfolder in src
+
+2) Create context object JS file in the contexts subfolder
+
+  - eg. ProductContext.js (must be capitalized because it is an object)
+
+3) In the context JS object...
+
+  - import createContext from react library
+
+  [] import { createContext } from 'react';
+
+  [] create the context object
+
+    export const ProductContext = createContext();
+
+    NOTE:  We initialize later, when providing the context to a component tree.
