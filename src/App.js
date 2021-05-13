c088be9b41bfcecd9c04ddaa2f739d6a4bd00d35
Kaseem Bradley <kaseembradley@Kaseems-Air.fios-router.home>
2021-05-13 18:02:16 -0400

    created and implemented all context

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route } from "react-router-dom";
 import data from "./data";
 
 import { ProductContext } from "../src/contexts/ProductContext";
+import { CartContext } from "../src/contexts/CartContext";
 
 // Components
 import Navigation from "./components/Navigation";
@@ -20,18 +21,20 @@ function App() {
 
   return (
     <div className="App">
-      <ProductContext.Provider value={{ products, addItem }}>
-        <Navigation cart={cart} />
+      <CartContext.Provider value={cart}>
+        <Navigation />
 
         {/* Routes */}
-        <Route exact path="/">
-          <Products products={products} addItem={addItem} />
-        </Route>
+        <ProductContext.Provider value={{ products, addItem }}>
+          <Route exact path="/">
+            <Products />
+          </Route>
 
-        <Route path="/cart">
-          <ShoppingCart cart={cart} />
-        </Route>
-      </ProductContext.Provider>
+          <Route path="/cart">
+            <ShoppingCart cart={cart} />
+          </Route>
+        </ProductContext.Provider>
+      </CartContext.Provider>
     </div>
   );
 }
--- src/contexts/ProductContext.js
+++ src/contexts/ProductContext.js
@@ -1,4 +1,3 @@
-import React, { createContext } from "react";
-import data from "../data";
+import { createContext } from "react";
 
 export const ProductContext = createContext();
