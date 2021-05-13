9bf140b86b308ee2dbc7b547002f8207a8688c0f
ricardo-pena <ricardo.penagarcia06@gmail.com>
2021-05-13 10:59:09 -0400

    MVP

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
+import ProductContex from './context/ProductContext';
+import CartContex from './context/CartContext';
+
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
@@ -13,21 +16,30 @@ function App() {
 
 	const addItem = item => {
 		// add the given item to the cart
+		setCart([...cart,item])
 	};
 
 	return (
+		<ProductContex.Provider value = {{products,addItem}}>
+		<CartContex.Provider value = {{cart}}>
+
+
 		<div className="App">
 			<Navigation cart={cart} />
 
 			{/* Routes */}
 			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
+				<Products/>
 			</Route>
 
 			<Route path="/cart">
 				<ShoppingCart cart={cart} />
 			</Route>
 		</div>
+
+		
+		</CartContex.Provider>
+		</ProductContex.Provider>
 	);
 }
 
