52b5bcb08a852e79e7d5bfc8be9578bdf0f85b78
Cynthia Coronado <cynthia.softball12@gmail.com>
2021-07-29 19:11:41 -0700

    Task three done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
+import { ProductContext } from './contexts/ProductContext'
 
 // Components
 import Navigation from './components/Navigation';
@@ -17,16 +18,18 @@ function App() {
 
 	return (
 		<div className="App">
+		<ProductContext.Provider value ={{products, addItem}}>
 			<Navigation cart={cart} />
 
 			{/* Routes */}
 			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
+				<Products />
 			</Route>
 
 			<Route path="/cart">
 				<ShoppingCart cart={cart} />
 			</Route>
+			</ProductContext.Provider>
 		</div>
 	);
 }
