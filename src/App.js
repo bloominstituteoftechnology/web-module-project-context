8cda6dcd6dd5c50220a375783fbd2a30a3654003
bverfurth <78745926+bverfurth@users.noreply.github.com>
2021-08-12 16:26:56 -0400

    Final Push - All Done

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Navigation from "./components/Navigation";
 import Products from "./components/Products";
 import ShoppingCart from "./components/ShoppingCart";
 
+// State Properties
 function App() {
   const [products] = useState(data);
   const [cart, setCart] = useState([]);
