fa683a152886d7fb4594216944f94c99d7e32b8f
Rick <ricksmycodeguy@gmail.com>
2021-09-09 14:59:06 -0500

    Step 4 complete

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,20 +1,23 @@
+import { useContext } from 'react';
+import { ProductContext } from '../contexts/ProductContext';
 import React from 'react';
 
-// Components
-import Product from './Product';
-
-const Products = props => {
+const Product = () => {
+	//In the component, call the `useContext` hook and pass in the context object we want to use into it.
+	const {product, addItem} = useContext(ProductContext)
 	return (
-		<div className="products-container">
-			{props.products.map(product => (
-				<Product
-					key={product.id}
-					product={product}
-					addItem={props.addItem}
-				/>
-			))}
+		<div className="product">
+			<img src={product.image} alt={`${product.title} book`} />
+
+			<h1 className="title">{product.title}</h1>
+
+			<p className="price">${product.price}</p>
+
+			<button onClick={() => addItem(product)}>
+				Add to cart
+			</button>
 		</div>
 	);
 };
 
-export default Products;
+export default Product;
