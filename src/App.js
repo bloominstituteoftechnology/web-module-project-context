de5cd76e8974adfbde132322a41001dad882e51f
Klove Adams <kalebsxott@gmail.com>
2021-09-09 18:15:25 -0400

    Consuming data with ProductContext

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+import ProductContext from "./contexts/ProductContext";
+
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
@@ -20,13 +22,15 @@ function App() {
 			<Navigation cart={cart} />
 
 			{/* Routes */}
+      <ProductContext.Provider value={{ products, addItem }}>
 			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
+				<Products />
 			</Route>
 
 			<Route path="/cart">
 				<ShoppingCart cart={cart} />
 			</Route>
+      </ProductContext.Provider>
 		</div>
 	);
 }
