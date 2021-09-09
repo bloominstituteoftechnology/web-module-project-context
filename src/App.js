06c6c1de82e3c2f23506d5cd74f7e934f9b64a1a
Rick <ricksmycodeguy@gmail.com>
2021-09-09 15:05:48 -0500

    Step 6. complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
-
+import { CartContext } from './contexts/CartContext';
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
