5ecd5f93b469675a37982fba5cbeb7f42f5fac37
Rick <ricksmycodeguy@gmail.com>
2021-09-09 14:51:45 -0500

    Step 3. complete

--- src/App.js
+++ src/App.js
@@ -20,17 +20,20 @@ function App() {
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
+		<ProductContext.Provider value={{products, addItem}}>
+			<CartContext.Provider value={cart}>
+				<Navigation />
+					{/* Routes */}
+					<Route exact path="/">
+						<Products />
+					</Route>
 
-			{/* Routes */}
-			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
-
-			<Route path="/cart">
-				<ShoppingCart cart={cart} />
-			</Route>
-		</div>
+					<Route path="/cart">
+						<ShoppingCart />
+					</Route>
+			</CartContext.Provider>
+		</ProductContext.Provider>
+	</div>
 	);
 }
 
