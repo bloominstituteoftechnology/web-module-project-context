66e3602fa7a8d8dfbc8db250407a28a7f72cf932
Justin Peisker <justinpeisker@gmail.com>
2021-11-04 12:43:46 -0700

    still buggy

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { NavLink } from 'react-router-dom';
+import ProductContext from '../context/ProductContext';
+
 
 const Navigation = props => {
+	const { cart } = useContext(ProductContext)
 	return (
 		<div className="navigation">
 			<NavLink to="/">Products</NavLink>
 			<NavLink to="/cart">
-				Cart <span>{props.cart.length}</span>
+				Cart <span>{cart.length}</span>
 			</NavLink>
 		</div>
 	);
