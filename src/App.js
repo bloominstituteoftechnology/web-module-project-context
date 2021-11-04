ffb0e3203abf8447ecabec1ec65bc4ba6e7934ab
Alex Hoskins <alexander.hoskins@yahoo.com>
2021-11-04 12:04:39 -0700

    Working on removeItem but currently not functioning

--- src/App.js
+++ src/App.js
@@ -18,9 +18,17 @@ function App() {
 		setCart([...cart, item])
 	};
 
+	const removeItem = id =>{
+		setCart([cart.filter(item=>{
+			if(item.id === !id){
+				return item
+			}}
+		)])
+	}
+
 	return (
 		<ProductContext.Provider value={{products, addItem}}>
-			<CartContext.Provider value={{cart}}>
+			<CartContext.Provider value={{cart, removeItem}}>
 				<div className="App">
 					<Navigation />
 
--- src/components/ShoppingCartItem.js
+++ src/components/ShoppingCartItem.js
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, {useContext} from "react";
 
-const Item = props => {
+import CartContext from './../contexts/CartContext'
+
+const Item = (props) => {
+	const { removeItem } = useContext(CartContext)
+	console.log('props from item',props)
+	const handleClick = () =>{
+		removeItem(props.id)
+		console.log(props.id)
+	}
 	return (
 		<div className="shopping-cart_item">
 			<img src={props.image} alt={`${props.title} book`} />
@@ -9,7 +17,7 @@ const Item = props => {
 			<div>
 				<h1>{props.title}</h1>
 				<p>$ {props.price}</p>
-				<button>Remove from cart</button>
+				<button onClick={handleClick}>Remove from cart</button>
 			</div>
 		</div>
 	);
