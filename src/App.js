da2019046ffa6e43241ed2e8e05fda8a4bc408d6
sooof <sooof@me.com>
2021-11-05 00:23:02 -0700

    Navigation

--- src/App.js
+++ src/App.js
@@ -24,7 +24,8 @@ function App() {
 		<ProductContext.Provider value={{products, addItem}}>
 			<CartContext.Provider value={cart}>
 				<div className="App">
-					<Navigation cart={cart} />
+					{/* <Navigation cart={cart} /> */}
+					<Navigation  />
 
 					{/* Routes */}
 					<Route exact path="/">
