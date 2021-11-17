88c05f3bc40f998b4ada2672ce0a87bd7b31943e
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-11-17 11:14:01 -0500

    adds basic info.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, withRouter } from 'react-router-dom';
-import './sass/index.scss';
+import './css/global.css';
+import './css/Navigation.css';
+import './css/Product.css';
+import './css/Products.css';
+import './css/ShoppingCart.css';
+
 import App from './App';
 
 const AppWithRouter = withRouter(App);
