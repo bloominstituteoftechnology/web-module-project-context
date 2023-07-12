4f59f50096ccd5b57fe3d8bd1602576ef5296456
DJ1 <jimmynoble604@gmail.com>
2023-07-11 21:42:06 -0400

    completed

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import ProductContext from './../contexts/ProductContext';
 
 const Product = props => {
-	const values = useContext(ProductContext);
+	const { addItem } = useContext(ProductContext);
 	return (
 		<div className="product">
 			<img src={props.product.image} alt={`${props.product.title} book`} />
@@ -11,7 +11,7 @@ const Product = props => {
 
 			<p className="price">${props.product.price}</p>
 
-			<button onClick={() => props.addItem(props.product)}>
+			<button onClick={() => addItem(props.product)}>
 				Add to cart
 			</button>
 		</div>
